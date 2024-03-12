1065ac82c106140c209b88c4439b2df9a026c15d
orbu13 <orelbutbul13@gmail.com>
2024-03-12 16:09:20 -0400

    homework

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//homework page 28 (14)
+let x = [1,3,0,4];
+console.log(x.reverse(x));
+
+let y = 123
+console.log(y.toString().c)
+let p = y.toString().charAt(0)
+let c = y.toString().charAt(1)
+let j = y.toString().charAt(2)
+console.log(p);
+console.log(c);
+console.log(j);
+
+let combainString = j+c+p;
+console.log(combainString);
